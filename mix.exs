0e88e010431860241c621dc352303dd1fdf9f838
Joel Koch <joel@joelkoch.dev>
2025-06-15 20:25:56 +0200

    mix release in prod env

--- mix.exs
+++ mix.exs
@@ -12,6 +12,12 @@ defmodule Bumbur.MixProject do
     ]
   end
 
+  def cli do
+    [
+      preferred_envs: [release: :prod]
+    ]
+  end
+
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
